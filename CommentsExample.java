dbd1cbfac4448c8ce180a9265e396c93dbf314b9
puppy2812 <rajapanthulajagadeeshwari@gmail.com>
2026-02-28 14:20:32 +0530

    Update CommentsExample.java

--- CommentsExample.java
+++ CommentsExample.java
@@ -2,7 +2,7 @@ public class CommentsExample {
     public static void main(String[] args) {
         System.out.println("Hello, World");
         int x = 10,y=2;
-        System.out.println("Value of x: " +(x-y));
-        System.out.println("Value of x: " +(x+y));
+        System.out.println("Value of a: " +(x-y));
+        System.out.println("Value of b: " +(x+y));
     }
 }
